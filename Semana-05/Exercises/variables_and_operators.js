15d38d6a7c511354b62e42334685b20c9215066d
Dayana Guerra <dayana96gm@gmail.com>
2022-04-15 16:50:40 -0300

    BaSP-2022: adding variables and operators exercises

---
+++ Semana-05/Exercises/variables_and_operators.js
@@ -0,0 +1,23 @@
+// 1.
+var slicesOfPizzaEaten, slicesOfPizzaLeft, entirePizzaSlices;
+slicesOfPizzaEaten = 3;
+slicesOfPizzaLeft = 5;
+entirePizzaSlices = slicesOfPizzaEaten + slicesOfPizzaLeft;
+
+console.log(entirePizzaSlices);
+
+// 2. 
+var firstName, favFood;
+firstName = 'Dayana';
+favFood = 'pizza';
+
+var aboutMe = "Hi! I'm " + firstName + " and my favorite food is " + favFood;
+console.log(aboutMe);
+
+// 3
+var favAnimal, favFood;
+favAnimal = 'tiger';
+favFood = 'pizza';
+
+var magicNumber = favAnimal.length + favFood.length;
+console.log(magicNumber);
