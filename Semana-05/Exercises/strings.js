42e21e326df581adc67e981dd21e812dee712ff4
Dayana Guerra <dayana96gm@gmail.com>
2022-04-16 10:33:44 -0300

    BaSP-2022: adding link to the html and strings exercises

---
+++ Semana-05/Exercises/strings.js
@@ -0,0 +1,50 @@
+// a
+var sentence = "this whole sentence should be uppercase";
+
+console.log(sentence.toUpperCase());
+
+// b
+var sentence2 = "Messi is my favorite football player";
+var firstFiveChar = sentence2.substring(0, 5);
+var newSentence = firstFiveChar + " is the best player in the world";
+
+console.log(newSentence);
+
+// c
+var sentence3 = "There is no better horror writer than Poe";
+var lastThreeChar = sentence3.substring(38);
+var newSentence2 = lastThreeChar + " was born in Boston";
+
+console.log(newSentence2);
+
+// d
+var quote1 = "life is what happens when you're busy making other plans";
+var firstChar = quote1.substring(0,1);
+var capitalLetter = firstChar.toUpperCase();
+var quote2 = " IS FOR LONDON";
+var lowerQuote = quote2.toLowerCase();
+var finalResult = capitalLetter + lowerQuote;
+
+console.log(finalResult);
+
+// e
+var sentence4 = "cats are the best";
+var firstSpace = sentence4.indexOf(" ");
+
+console.log(firstSpace);
+
+// f
+var longWords = "photojournalism electromagnetic";
+var firstLetter = longWords.substring(0, 1);
+var positionSecondLetter = longWords.indexOf("e");
+var secondLetter = longWords.substring(16, 17);
+
+var sentence5 = "STANDS FOR PHYSICAL EDUCATION";
+
+var finalSentence = firstLetter.toUpperCase() + secondLetter.toUpperCase() + " " + sentence5.toLowerCase();
+console.log(finalSentence);
+
+
+
+
+
