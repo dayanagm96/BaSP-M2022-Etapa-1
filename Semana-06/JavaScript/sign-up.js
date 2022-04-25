0991d05ab258781f449b66695ae83baba009adcb
Dayana Guerra <dayana96gm@gmail.com>
2022-04-24 22:17:17 -0300

    BaSP-2022: changing tags and adding js file

---
+++ Semana-06/JavaScript/sign-up.js
@@ -0,0 +1,20 @@
+console.log('working');
+inputs
+var name = document.getElementById('name');
+var lastName = document.getElementById('last-name');
+var idNumber = document.getElementById('id-number');
+var dateBirth = document.getElementById('date-birth');
+var phoneNumber = document.getElementById('phone-num');
+var address = document.getElementById('address');
+var location = document.getElementById('location');
+var zipCode = document.getElementById('zip-code');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+var confirmPassword = document.getElementById('confirm-password');
+
+// form
+var form = document.getElementById('form-input');
+// validation colors
+var red = '#F44336';
+var green = '#4CAF50';
+
